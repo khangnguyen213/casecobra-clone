11476cda8ca7a26a52b3d5ec44b384794b292118
Khang Nguyen <khangnguyenm01@gmail.com>
2024-06-05 10:16:37 +0700

    update a little

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,25 @@ export default function Home() {
     <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="lg:grid lg:grid-cols-3 pb-24 sm:pb-32 lg:pb-52 lg:gap-x-0 xl:gap-x-8 pt-10 lg:pt-24 xl:pt-32 ">
-          ok
+          <div className="col-span-2 px-6 lg:px-0 lg: pt-0">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image Image on a {` `}
+                <span className="bg-green-600 px-2 text-white">Custom</span>
+                {` `}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-cemter lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,{' '}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you to protect your memories, not just your
+                phone case.
+              </p>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
